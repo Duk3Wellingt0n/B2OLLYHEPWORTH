76fb69753014d1be521b0a1c4588ca5e197c968c
Duk3Wellingt0n <ghoulolly@outlook.com>
2026-02-23 06:42:17 +0000

    start of dialogue system

---
+++ B2_OllyHepworth/Source/B2_OllyHepworth/Public/OratorDataStructs.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class B2_OLLYHEPWORTH_API OratorDataStructs
+{
+public:
+	OratorDataStructs();
+	~OratorDataStructs();
+};
